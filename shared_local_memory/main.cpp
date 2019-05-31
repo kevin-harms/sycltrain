88c8757ce4cfc0d2ba39d37483c030325a99ea49
Kevin Harms <harms@alcf.anl.gov>
2019-05-31 05:28:37 +0000

    update slm example

--- shared_local_memory/main.cpp
+++ shared_local_memory/main.cpp
@@ -2,55 +2,35 @@
 #include <CL/sycl.hpp>
 #include <iostream>
 
-#define SIZE 8
+#define SIZE 256
 
-void print_buffer (cl::sycl::accessor<int, 1, cl::sycl::access::mode::write, cl::sycl::access::target::host_buffer> &acc)
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        std::cout << acc[i] << std::endl;
-    }
-}
 
 int main (int argc, char **argv)
 {
     const cl::sycl::device_selector &cpu_dev = cl::sycl::cpu_selector();
-    cl::sycl::queue q;
-    cl::sycl::buffer<int, 1> b(cl::sycl::range<1>(SIZE));
-    cl::sycl::accessor<int, 1, cl::sycl::access::mode::write, cl::sycl::access::target::host_buffer> ha_b = b.get_access<cl::sycl::access::mode::write>();
-    for (int i = 0; i < SIZE; i++)
-    {
-        ha_b[i] = 0;
-    }
-    
-    print_buffer(ha_b);
+    const cl::sycl::device_selector &gpu_dev = cl::sycl::gpu_selector();
+    cl::sycl::queue q(gpu_dev);
 
     q.submit([&](cl::sycl::handler &h)
         {
             cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::local> acc = cl::sycl::accessor<int, 1,  cl::sycl::access::mode::read_write, cl::sycl::access::target::local>(cl::sycl::range<1>(SIZE), h);
-	   // auto da_b = b.get_access<cl::sycl::access::mode::write>(h);
 
-            h.parallel_for<class kernel1>(cl::sycl::range<1>(SIZE),
-                [=](cl::sycl::id<1> i)
+            h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(SIZE), cl::sycl::range<1>(SIZE)),
+                [=](cl::sycl::nd_item<1> i)
                 {
-                    int x = i[0];
+                    int x = i.get_global_linear_id();
                     int y;
                     acc[x] = x;
-                    for (int j = 0, y = 0; j < 1000000; j++)
+                    if ((x < (SIZE-1)) &&
+                        (acc[x+1] != (x+1)))
                     {
-                        y = y + j;
-                    }
-                    if (acc[x] != x)
-                    {
-                        //da_b[x] = 1;
+                        printf("unexpected value: %d %d\n", acc[x+1], x+1);
                     }
                 }
             );
         }
     );
     q.wait();
-
-    print_buffer(ha_b);
 
     return 0;
 }
