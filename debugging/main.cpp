62266c3987f3c2f7111b9ca7004f2c21f2a8a3e4
Kevin Harms <harms@alcf.anl.gov>
2019-05-31 05:05:26 +0000

    fix debug example

--- debugging/main.cpp
+++ debugging/main.cpp
@@ -4,36 +4,58 @@
 
 void display_device(const cl::sycl::device_selector &selector);
 
+#define SIZE 16
+
+void print_buffer (cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> &acc)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        std::cout << acc[i] << std::endl;
+    }
+}
+
 int main (int argc, char **argv)
 {
 
-    const cl::sycl::device_selector &cpu_dev = cl::sycl::cpu_selector();
-    const cl::sycl::device_selector &gpu_dev = cl::sycl::gpu_selector();
+    const cl::sycl::device_selector &dev = cl::sycl::gpu_selector();
 
     cl::sycl::async_handler ah = \
-        [](cl::sycl::exception_list elist) { for( auto &e : elist) { std::cout << "Async Exception: " << e.what() << std::endl; } };
+        [](cl::sycl::exception_list elist)
+        {
+            for( auto &e : elist)
+            {
+                try { std::rethrow_exception(e); }
+                catch (cl::sycl::exception &ce) { std::cout << "Async Exception: " << ce.what() << std::endl; }
+                catch (std::exception &se) { std::cout << "Async Exception: " << se.what() << std::endl; }
+            }
+            exit(1);
+        };
 
-    display_device(cpu_dev);
-    cl::sycl::queue q(cpu_dev, ah);
-
-    cl::sycl::buffer<int, 1> b(cl::sycl::range<1>(SIZE));
-    cl::sycl::accessor<int, 1, cl::sycl::access::mode::write, cl::sycl::access::target::host_buffer> ha_b = b.get_access<cl::sycl::access::mode::write>();
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        ha_b[i] = 0;
-    }
-
-    print_buffer(ha_b);
+    std::vector<int> v(SIZE);
 
     try
     {
+
+        cl::sycl::queue q(dev, ah);
+        display_device(dev);
+
+        cl::sycl::buffer<int, 1> b { cl::sycl::range<1>(SIZE) };
+        cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> ha_b = b.get_access<cl::sycl::access::mode::read_write>();
+
+        for (int i = 0; i < SIZE; i++)
+        {
+             ha_b[i] = 1;
+        }
+
+        print_buffer(ha_b);
+        std::cout << "run kernel" << std::endl;
+
         q.submit([&](cl::sycl::handler &h)
         {
-             cl::sycl::accessor<int, 1, cl::sycl::access::mode::write, cl::sycl::access::target::local> acc = cl::sycl::accessor<int, 1,  cl::sycl::access::mode::write, cl::sycl::access::target::local>(cl::sycl::range<1>(SIZE), h);
             auto da_b = b.get_access<cl::sycl::access::mode::write>(h);
 
-            h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(100000), cl::sycl::range<1>(10)),
+            h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(16), cl::sycl::range<1>(1)),
+            //h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(5), cl::sycl::range<1>(2)),
                 [=](cl::sycl::nd_item<1> i)
                 {
                     size_t idx = i.get_global_linear_id();
@@ -42,15 +64,17 @@ int main (int argc, char **argv)
             );
         }
         );
-        q.wait();
-        // q.wait_and_throw();
+        q.wait_and_throw();
+        //q.wait();
+
+        cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> ha2_b = b.get_access<cl::sycl::access::mode::read_write>();
+        print_buffer(ha2_b);
     }
     catch (cl::sycl::exception &e)
     {
         std::cout << "Exception: " << e.what() << std::endl;
     }
 
-    print_buffer(ha_b);
 
     return 0;
 }
