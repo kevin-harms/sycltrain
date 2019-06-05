35c4391e61c76bb860199ceb7af49090d4bd3d3c
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2019-06-05 11:35:12 -0500

    Update 4_buffer.cpp

--- 7_sycl_of_hell/4_buffer.cpp
+++ 7_sycl_of_hell/4_buffer.cpp
@@ -1,33 +1,18 @@
 #include <CL/sycl.hpp>
 
-class hello_write;
-template< class T>
-void foo(cl::sycl::handler& cgh, int global_range, int local_range,   T accessorA)
-{
-    // Nd range allow use to access information
-    cgh.parallel_for<class hello_write>(cl::sycl::nd_range<1>{cl::sycl::range<1>(global_range),
-                                                             cl::sycl::range<1>(local_range) },
-                                        [=](cl::sycl::nd_item<1> idx) {
-      const int world_rank = idx.get_global_id(0);
-      accessorA[world_rank] = world_rank;
-    });
-}
-
 // Inspired by Codeplay compute cpp hello-world
 int main(int argc, char** argv) {
 
    const auto global_range =  (size_t) atoi(argv[1]);
    const auto local_range =  (size_t) atoi(argv[2]);
-   const auto write = (bool) atoi(argv[3]);
 
-   // Create array
+
+   // Crrate array
    int A[global_range];
 
   // Selectors determine which device kernels will be dispatched to.
-  cl::sycl::gpu_selector selector; 
+  cl::sycl::default_selector selector; 
   // Create your own or use `{cpu,gpu,accelerator}_selector`
-  
-
   {
   // Create sycl buffer.
   // Trivia: What happend if we create the buffer in the outer scope?
@@ -41,18 +26,17 @@ int main(int argc, char** argv) {
   //Create a command_group to issue command to the group
   myQueue.submit([&](cl::sycl::handler& cgh) {
 
-
-     if (write) {
-     std::cout << "Using write accesor"<< std::endl;
-     auto accessorA = bufferA.get_access<cl::sycl::access::mode::write>(cgh);
-     foo(cgh, global_range, local_range, accessorA);
-    } else {
-     std::cout << "Using discard write accesor"<< std::endl;
      //Create an accesor for the sycl buffer. Trust me, use auto.
      auto accessorA = bufferA.get_access<cl::sycl::access::mode::discard_write>(cgh);
-     foo(cgh, global_range, local_range, accessorA);
-    }
 
+    // Nd range allow use to access information
+    cgh.parallel_for<class hello_world>(cl::sycl::nd_range<1>{cl::sycl::range<1>(global_range), 
+                                                             cl::sycl::range<1>(local_range) }, 
+                                        [=](cl::sycl::nd_item<1> idx) {
+      const int world_rank = idx.get_global_id(0);
+      accessorA[world_rank] = world_rank;
+
+    }); // End of the kernel function
   }); // End of the queue commands
   }  // End of scope, wait for the queued work to stop. 
      // Can also use  myQueue.wait_and_throw();
