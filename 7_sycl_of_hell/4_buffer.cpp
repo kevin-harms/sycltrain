10d0266cea61343dcb484146abc584026bce4f4e
TApplencourt <t.applencourt@anl.gov>
2019-06-05 16:45:41 +0000

    Typo

--- 7_sycl_of_hell/4_buffer.cpp
+++ 7_sycl_of_hell/4_buffer.cpp
@@ -6,7 +6,6 @@ int main(int argc, char** argv) {
    const auto global_range =  (size_t) atoi(argv[1]);
    const auto local_range =  (size_t) atoi(argv[2]);
 
-
    // Crrate array
    int A[global_range];
 
@@ -25,17 +24,14 @@ int main(int argc, char** argv) {
 
   //Create a command_group to issue command to the group
   myQueue.submit([&](cl::sycl::handler& cgh) {
-
      //Create an accesor for the sycl buffer. Trust me, use auto.
      auto accessorA = bufferA.get_access<cl::sycl::access::mode::discard_write>(cgh);
-
     // Nd range allow use to access information
     cgh.parallel_for<class hello_world>(cl::sycl::nd_range<1>{cl::sycl::range<1>(global_range), 
                                                              cl::sycl::range<1>(local_range) }, 
                                         [=](cl::sycl::nd_item<1> idx) {
       const int world_rank = idx.get_global_id(0);
       accessorA[world_rank] = world_rank;
-
     }); // End of the kernel function
   }); // End of the queue commands
   }  // End of scope, wait for the queued work to stop. 
