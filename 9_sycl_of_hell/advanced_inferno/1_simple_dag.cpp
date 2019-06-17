308be01495c18d6596bbf48eb22c027192f1b537
TApplencourt <t.applencourt@anl.gov>
2019-06-17 22:34:32 +0000

    Add reference counting test

--- 9_sycl_of_hell/advanced_inferno/1_simple_dag.cpp
+++ 9_sycl_of_hell/advanced_inferno/1_simple_dag.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 #include <CL/sycl.hpp>
 
-template <typename dataW, typename dataR>
+template <typename TAccessorW, typename TAccessorR>
 class memcopy_kernel{
  public:
-   memcopy_kernel(dataW accessorW_, dataR accessorR_): accessorW{accessorW_},  accessorR{accessorR_}  {}
+   memcopy_kernel(TAccessorW accessorW_, TAccessorR accessorR_): accessorW{accessorW_},  accessorR{accessorR_}  {}
    void operator()(cl::sycl::item<1> idx) { accessorW[idx.get_id()] = accessorR[idx.get_id()] + 1; }
  private:
-   dataW accessorW;
-   dataR accessorR;
+   TAccessorW accessorW;
+   TAccessorR accessorR;
 };
 
 void f_copy(cl::sycl::handler& cgh, int global_range, cl::sycl::buffer<cl::sycl::cl_int, 1> bufferW, 
@@ -26,6 +26,11 @@ int main(int argc, char** argv) {
    // Crrate array
    std::vector<int> A(global_range, 0);
 
+  // Create buffer
+  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferA(A.data(), global_range);
+  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferB(global_range);
+  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferC(global_range);
+
   // Selectors determine which device kernels will be dispatched to.
   cl::sycl::default_selector selector;
   // Create your own or use `{cpu,gpu,accelerator}_selector`
@@ -34,19 +39,12 @@ int main(int argc, char** argv) {
   std::cout << "Running on "
             << myQueue.get_device().get_info<cl::sycl::info::device::name>()
             << "\n";
-  
   //   _      __ 
   //  | \ /\ /__ 
   //  |_//--\\_| 
-
-  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferA(A.data(), global_range);
-  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferB(global_range);
-  cl::sycl::buffer<cl::sycl::cl_int, 1> bufferC(global_range);
-
   myQueue.submit(std::bind(f_copy, std::placeholders::_1, global_range, bufferB, bufferA));
   myQueue.submit(std::bind(f_copy, std::placeholders::_1, global_range, bufferC, bufferB));
   myQueue.submit(std::bind(f_copy, std::placeholders::_1, global_range, bufferA, bufferC));
-
   }  // End of scope, wait for the queued work to stop.
  
  for (size_t i = 0; i < global_range; i++)
