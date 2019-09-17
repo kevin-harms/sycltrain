226be8c77974e771a3ebd2ad669abdc7007f2d8d
Kevin Harms <harms@alcf.anl.gov>
2019-09-17 16:15:22 +0000

    Update error handling example

--- debugging/diag.cpp
+++ debugging/diag.cpp
@@ -3,26 +3,28 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void display_device(const cl::sycl::device_selector &selector)
-{
-    cl::sycl::platform platform = cl::sycl::platform(selector);
-    std::vector<cl::sycl::device> devices = platform.get_devices();
+using namespace cl;
 
-    std::cout << platform.get_info<cl::sycl::info::platform::name>() << std::endl;
-    std::cout << "\t" << platform.get_info<cl::sycl::info::platform::version>() << std::endl;
+void display_device(const sycl::device_selector &selector)
+{
+    sycl::platform platform = sycl::platform(selector);
+    std::vector<sycl::device> devices = platform.get_devices();
+
+    std::cout << platform.get_info<sycl::info::platform::name>() << std::endl;
+    std::cout << "\t" << platform.get_info<sycl::info::platform::version>() << std::endl;
     for (auto const device : devices)
     {
         if (device.is_gpu())
         {
-            std::cout << "\t" << "name: " << device.get_info<cl::sycl::info::device::name>() << std::endl;
-            std::cout << "\t" << "eu: " << device.get_info<cl::sycl::info::device::max_compute_units>() << std::endl;
-            std::cout << "\t" << "vector: " << device.get_info<cl::sycl::info::device::preferred_vector_width_char>() << std::endl;
-            std::cout << "\t" << "local mem: " << device.get_info<cl::sycl::info::device::local_mem_size>() << std::endl;
+            std::cout << "\t" << "name: " << device.get_info<sycl::info::device::name>() << std::endl;
+            std::cout << "\t" << "eu: " << device.get_info<sycl::info::device::max_compute_units>() << std::endl;
+            std::cout << "\t" << "vector: " << device.get_info<sycl::info::device::preferred_vector_width_char>() << std::endl;
+            std::cout << "\t" << "local mem: " << device.get_info<sycl::info::device::local_mem_size>() << std::endl;
         }
         else
         {
-            std::cout << "\t" << "name: " << device.get_info<cl::sycl::info::device::name>() << std::endl;
-            std::cout << "\t" << "cores: " << device.get_info<cl::sycl::info::device::max_compute_units>() << std::endl;
+            std::cout << "\t" << "name: " << device.get_info<sycl::info::device::name>() << std::endl;
+            std::cout << "\t" << "cores: " << device.get_info<sycl::info::device::max_compute_units>() << std::endl;
         }
     }
 
--- debugging/main.cpp
+++ debugging/main.cpp
@@ -2,79 +2,83 @@
 #include <CL/sycl.hpp>
 #include <iostream>
 
-void display_device(const cl::sycl::device_selector &selector);
+using namespace cl;
+
+void display_device(const sycl::device_selector &selector);
 
 #define SIZE 16
 
-void print_buffer (cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> &acc)
+void print_buffer (sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::host_buffer> &acc)
 {
     for (int i = 0; i < SIZE; i++)
     {
-        std::cout << acc[i] << std::endl;
+        std::cout << acc[i] << " ";
     }
+    std::cout << std::endl;
+
+    return;
 }
 
 int main (int argc, char **argv)
 {
 
-    const cl::sycl::device_selector &dev = cl::sycl::gpu_selector();
+    const sycl::device_selector &dev = sycl::gpu_selector();
 
-    cl::sycl::async_handler ah = \
-        [](cl::sycl::exception_list elist)
+    sycl::async_handler ah = \
+        [](sycl::exception_list elist)
         {
-            for( auto &e : elist)
+            for( auto e : elist)
             {
-                try { std::rethrow_exception(e); }
-                catch (cl::sycl::exception &ce) { std::cout << "Async Exception: " << ce.what() << std::endl; }
-                catch (std::exception &se) { std::cout << "Async Exception: " << se.what() << std::endl; }
+                std::rethrow_exception(e);
             }
-            exit(1);
         };
 
     std::vector<int> v(SIZE);
 
+    sycl::queue q(dev, ah);
+    display_device(dev);
+
+    sycl::buffer<int, 1> b { sycl::range<1>(SIZE) };
+    auto ha_b = b.get_access<sycl::access::mode::read_write>();
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        ha_b[i] = 1;
+    }
+
+    print_buffer(ha_b);
+    std::cout << "run kernel" << std::endl;
+
     try
     {
 
-        cl::sycl::queue q(dev, ah);
-        display_device(dev);
-
-        cl::sycl::buffer<int, 1> b { cl::sycl::range<1>(SIZE) };
-        cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> ha_b = b.get_access<cl::sycl::access::mode::read_write>();
-
-        for (int i = 0; i < SIZE; i++)
-        {
-             ha_b[i] = 1;
-        }
-
-        print_buffer(ha_b);
-        std::cout << "run kernel" << std::endl;
-
-        q.submit([&](cl::sycl::handler &h)
-        {
-            auto da_b = b.get_access<cl::sycl::access::mode::write>(h);
-
-            h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(16), cl::sycl::range<1>(1)),
-            //h.parallel_for<class kernel1>(cl::sycl::nd_range<1>(cl::sycl::range<1>(5), cl::sycl::range<1>(2)),
-                [=](cl::sycl::nd_item<1> i)
-                {
-                    size_t idx = i.get_global_linear_id();
-                    da_b[idx] = idx;
-                }
-            );
-        }
-        );
-        q.wait_and_throw();
-        //q.wait();
-
-        cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::host_buffer> ha2_b = b.get_access<cl::sycl::access::mode::read_write>();
-        print_buffer(ha2_b);
-    }
-    catch (cl::sycl::exception &e)
+    q.submit([&](sycl::handler &cgh)
     {
-        std::cout << "Exception: " << e.what() << std::endl;
-    }
+        auto da_b = b.get_access<sycl::access::mode::write>(cgh);
+        int *ptr = 0;
 
+        cgh.parallel_for<class kernel1>(sycl::nd_range<1>(sycl::range<1>(16),
+                                                          sycl::range<1>(1)),
+        [=](sycl::nd_item<1> i)
+        {
+            size_t idx = i.get_global_linear_id();
+            *ptr = 0;
+            da_b[idx] = idx;
+        });
+    });
+
+    // Observe difference in behavior between wait and wait_and_throw
+    // q.wait();
+    q.wait_and_throw();
+
+    auto ha2_b = b.get_access<cl::sycl::access::mode::read_write>();
+    print_buffer(ha2_b);
+
+    }
+    catch (sycl::exception &e)
+    {
+        std::cout << "Async Exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
