6049d8eec2c4b37fe522953f98620e272a7d4e6b
tapplencourt <tapplencourt@anl.gov>
2020-02-03 15:56:39 -0800

    Cleaning. Put kevin example in main example repo

--- 9_sycl_of_hell/7_buffer_usm.cpp
+++ 9_sycl_of_hell/7_buffer_usm.cpp
@@ -5,7 +5,6 @@ namespace sycl = cl::sycl;
 int main(int argc, char** argv) {
 
    const auto global_range =  (size_t) atoi(argv[1]);
-   const auto local_range =  (size_t) atoi(argv[2]);
 
   // Selectors determine which device kernels will be dispatched to.
   sycl::default_selector selector;
@@ -16,16 +15,18 @@ int main(int argc, char** argv) {
 
   int* A = static_cast<int*>(sycl::malloc_shared(global_range*sizeof(int), dev, ctex));
 
+    // Advise runtime how memory will be used
+  auto e = myQueue.mem_advise(A, global_range*sizeof(int), 0);
+  e.wait();
+
   std::cout << "Running on "
             << myQueue.get_device().get_info<sycl::info::device::name>()
             << "\n";
 
   //Create a command_group to issue command to the group
   myQueue.submit([&](sycl::handler& cgh) {
-     //Create an accesor for the sycl buffer. Trust me, use auto.
-    // Nd range allow use to access information
-    cgh.parallel_for<class hello_world>(sycl::nd_range<1>{sycl::range<1>(global_range), 
-                                                              sycl::range<1>(local_range) }, 
+    // No accessor needed!
+    cgh.parallel_for<class hello_world>(sycl::range<1>{sycl::range<1>(global_range) }, 
                                         [=](sycl::nd_item<1> idx) {
       const int world_rank = idx.get_global_id(0);
       A[world_rank] = world_rank;
